8237e728286e3c2bf7ba3aef4a1b177b15168c4f
Brenda <brendacassita@gmail.com>
2022-05-18 15:18:09 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
-  
+      <Test />
     </div>
   );
 }
