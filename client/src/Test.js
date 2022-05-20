00dafedcdd11e1bdb43e0598f48e5a63c13ab02c
Brenda <brendacassita@gmail.com>
2022-05-20 13:35:32 -0600

    applicants

--- client/src/Test.js
+++ client/src/Test.js
@@ -5,17 +5,30 @@ import axios from 'axios'
 
 const Test = () => {
   const [jobs, setJobs] = useState("")
+  const [applicants, setApplicants] = useState("")
   useEffect(()=>{
     getJobs()
+    getApplicants()
   }, [])
 
   const getJobs = async ()=>{
   try{
     let res = await axios.get('/api/jobs')
-    console.log('jobs:', jobs)
+    // console.log('jobs:', jobs)
+    console.log(res.data)
     setJobs(res.data)
   }catch (err){
     alert('err getting jobs')
+  }
+}
+
+const getApplicants = async () =>{
+  try{
+    let res = await axios.get('api/applicants')
+    console.log(res.data)
+    setApplicants(res.data)
+  }catch (err){
+    alert('err getting applicants')
   }
 }
 
